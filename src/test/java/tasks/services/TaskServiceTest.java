a865301d3efe25ce5e12bf5d64b5312334aa8ae6
Alexandru Sirbu <alexandru.sirbu@cloudflight.io>
2024-04-14 13:35:49 +0300

    Extract BBT test in other method

--- src/test/java/tasks/services/TaskServiceTest.java
+++ src/test/java/tasks/services/TaskServiceTest.java
@@ -6,55 +6,9 @@ import tasks.model.Task;
 
 import java.util.Date;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TaskServiceTest {
     private static TasksService service;
-
-    @BeforeEach
-    public void setUp() {
-        service.clear();
-        Task task = new Task("Test data", new Date(), new Date(), 1);
-        service.saveTask(task);
-    }
-
-    // ECP
-    @Test
-    public void saveTaskWithValidTitle() {
-        Task task = new Task("Sample", new Date(), new Date(), 1);
-
-        Assertions.assertDoesNotThrow(() -> service.saveTask(task));
-        Assertions.assertEquals(2, service.getTasks().size());
-    }
-
-    @Test
-    public void saveTaskWithInvalid() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Task task = new Task("bzhwbkwuzbzgntpfcdkrcnniztzzvjhitfwxrtphqjrgemnxuauyjuanvuieeeadgacfvxjamgypqxtkvaeidiyphmpabbrhhqaehhjmphecaemfmnvnqyryuxtpzzuifpnjmqwepbprcvuyhzfywrctikgyrhtuuawxdzkxjbcbqczcbeyvegkffmrafpxzruxfxjpmxrdftpuxihxrnkthwfjytyrgugxufqhmkezttkdkznzubdiygtbwvdxrghkzeetayzjduayfmqrgihmknknvhvvdmixmhzftjctw", new Date(), new Date(), 1);
-            service.saveTask(task);
-        });
-        Assertions.assertEquals(1, service.getTasks().size());
-    }
-
-    // BVA
-    @Test
-    public void saveTaskWithValidTitleBva() {
-        Task task = new Task("T", new Date(), new Date(), 1);
-
-        Assertions.assertDoesNotThrow(() -> {
-            service.saveTask(task);
-        });
-
-        Assertions.assertEquals(2, service.getTasks().size());
-        Assertions.assertEquals(service.getTasks().getTask(1), task);
-    }
-
-    @Test
-    public void saveTaskWithInvalidTitleUpperBound() {
-        Assertions.assertThrows(Exception.class, () -> {
-            Task task = new Task("wLhIUEnLeKNcsrv7oFQqpr2gznG41jQdaWE5MYp1x2Z88PEuaCegF3dSr3ScuuwzzFfT5Fj6Zah8etTUXrRiUau9qfLJZxqwqMoaDv6TT6mG8V2V20BrOlYlV1w2A50sREW5YFRA5N4cY1UMF7NMZN5KTeSCWskqlz5gKbdFbQaMj6P2ZZ3xqHlBH4eDfbrpKD6RJB3i4rASxpJ3RSNNMX0rG4uYJrnuCWlkFiEtjzd3yhVrkIlIwIpw4U9oH6Yt", new Date(), new Date(), 1);
-            service.saveTask(task);
-        });
-        Assertions.assertEquals(1, service.getTasks().size());
-    }
 
     @BeforeAll
     static void generalSetUp() {
@@ -115,13 +69,6 @@ public class TaskServiceTest {
             Assertions.assertEquals(2, service.getTasks().size());
             Assertions.assertEquals(service.getTasks().getTask(1), task);
         }
-
-//        @Test
-//        @DisplayName("Save task with invalid date")
-//        public void saveTaskWithInvalidDate() {
-//            Assertions.assertThrows(IllegalArgumentException.class, () -> new Task("Title", new Date(2025, 04, 26), new Date(), 1));
-//            Assertions.assertEquals(1, service.getTasks().size());
-//        }
     }
 
 
@@ -215,13 +162,6 @@ public class TaskServiceTest {
             Assertions.assertThrows(IllegalArgumentException.class, () -> new Task("Title", new Date(-1), new Date(69, 12, 31), 1));
             Assertions.assertEquals(1, service.getTasks().size());
         }
-
-//        @Test
-//        @DisplayName("Save task with invalid date (after 2024)")
-//        public void saveTaskWithInvalidDateUpperBound() {
-//            Assertions.assertThrows(IllegalArgumentException.class, () -> new Task("Title", new Date(2025, 1, 1), new Date(), 1));
-//            Assertions.assertEquals(1, service.getTasks().size());
-//        }
 
         @Test
         @Disabled("For testing purposes")
